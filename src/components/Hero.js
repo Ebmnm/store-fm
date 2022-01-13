8006635b16c285d942d2e5276b12c2e213f11a67
mike <ebmnm77@gmail.com>
2022-01-13 17:48:44 -0500

    home page

---
+++ src/components/Hero.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./styles/hero.scss"
+
+export default function Hero() {
+    return (
+        <div className='hero'>
+           <div className='hero-text'>
+            <h4>NEW PRODUCT</h4>
+            <h1>XX99 MARK II <br/> 
+            HEADPHONES</h1>
+
+            <p>Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast. </p>
+            <button> See Product</button>
+           </div>
+        </div>
+    )
+}
